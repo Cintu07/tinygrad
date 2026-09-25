691ad5a60701b09c67b76e2f4e10b2ed0fe3c62d
Cintu07 <pawankalyan1892@gmail.com>
2026-09-25 07:06:41 +0000

    test_ops: big trig inputs run on the qcom cl path, only ir3 skips them

--- test/backend/test_ops.py
+++ test/backend/test_ops.py
@@ -874,7 +874,7 @@ class TestOps(TensorTestCase):
     helper_test_op([(45,65)], lambda x: x.sin())
     helper_test_op([()], lambda x: x.sin())
     # works on real CUDA but not CI
-    if not ((DEV.interface.startswith("MOCK") and Device.DEFAULT in ("NV", "QCOM")) or Device.DEFAULT == "WEBGPU"):
+    if not ((DEV.interface.startswith("MOCK") and (Device.DEFAULT == "NV" or DEV.renderer == "IR3")) or Device.DEFAULT == "WEBGPU"):
       helper_test_op(None, lambda x: x.sin(), vals=[[math.nan, math.inf, -math.inf, 0.0]])
       helper_test_op(None, lambda x: x.sin(), vals=[[1e1, 1e2, 1e3, 1e4, 1e5, 1e6, -1e1, -1e2, -1e3, -1e4, -1e5, -1e6]],
                     atol=3e-3, rtol=3e-3, grad_atol=3e-3, grad_rtol=3e-3)
@@ -884,7 +884,7 @@ class TestOps(TensorTestCase):
     helper_test_op([(45,65)], lambda x: x.cos())
     helper_test_op([], lambda: torch.tensor(2.0).cos(), lambda: Tensor(2).cos(), forward_only=True)
     helper_test_op([()], lambda x: x.cos())
-    if not ((DEV.interface.startswith("MOCK") and Device.DEFAULT in ("NV", "QCOM")) or Device.DEFAULT == "WEBGPU"):
+    if not ((DEV.interface.startswith("MOCK") and (Device.DEFAULT == "NV" or DEV.renderer == "IR3")) or Device.DEFAULT == "WEBGPU"):
       helper_test_op(None, lambda x: x.cos(), vals=[[math.nan, math.inf, -math.inf, 0.0]])
       helper_test_op(None, lambda x: x.cos(), vals=[[1e1, 1e2, 1e3, 1e4, 1e5, 1e6, -1e1, -1e2, -1e3, -1e4, -1e5, -1e6]],
                     atol=3e-3, rtol=3e-3, grad_atol=3e-3, grad_rtol=3e-3)
@@ -895,7 +895,7 @@ class TestOps(TensorTestCase):
     helper_test_op([(45,65)], lambda x: x.tan(), low=-1.5, high=1.5)
     helper_test_op([(45,65)], lambda x: x.tan(), low=-5, high=5)
     helper_test_op([()], lambda x: x.tan())
-    if not ((DEV.interface.startswith("MOCK") and Device.DEFAULT in ("NV", "QCOM")) or Device.DEFAULT == "WEBGPU"):
+    if not ((DEV.interface.startswith("MOCK") and (Device.DEFAULT == "NV" or DEV.renderer == "IR3")) or Device.DEFAULT == "WEBGPU"):
       helper_test_op(None, lambda x: x.tan(), vals=[[math.nan, math.inf, -math.inf, 0.0]])
       helper_test_op(None, lambda x: x.tan(), vals=[[1e1, 1e2, 1e3, 1e4, 1e5, 1e6, -1e1, -1e2, -1e3, -1e4, -1e5, -1e6]],
                     atol=3e-3, rtol=3e-3, grad_atol=3e-3, grad_rtol=3e-3)
